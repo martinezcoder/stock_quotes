ac8b78864bcdddda676cf86c42c583615278e33e
Fran J Martinez <martinezcoder@gmail.com>
2014-09-24 20:44:49 +0200

    with urls

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,27 +1,22 @@
 module Api
   module V1
     class QuotesController < ApplicationController
-      class Quote < ::Quote
-        def as_json(options = {})
-          super.merge({created_on: created_at.to_datetime,
-                       updated_on: updated_at.to_datetime})
-        end
-      end
 
       respond_to :json
 
       def index
-        if params[:tag]
-          @quotes = Quote.tagged_with(params[:tag])
-        else
-          @quotes = Quote.all
+        quotes = Quote.select("id,author,text").all.as_json
+        quotes.each do |q|
+          q[:quote_url] = api_v1_quote_url(id: q["id"])
         end
-        respond_with @quotes
+        respond_with  quote_list: quotes
       end
 
       def show
-        respond_with Quote.find(params[:id])
+        @quote = Quote.find(params[:id])
+        respond_with @quote.attributes.merge({path: api_v1_quote_url})
       end
+
 
     end
   end
